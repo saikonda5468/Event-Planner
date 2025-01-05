4e0bb99d05c758db2b50e3a1bcee0447e7ae91fd
saikonda <saikondakrishna@gmail.com>
2025-01-05 18:32:00 -0500

    Initial commit

--- agents.py
+++ agents.py
@@ -10,9 +10,9 @@ from dotenv import load_dotenv
 litellm.set_verbose=True  # 👈 this is the 1-line change you need to make
 load_dotenv(override=True)
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
-os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+# os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 
 # Initialize the tools
